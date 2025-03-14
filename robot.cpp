3797f5c786c70762f4ddd2ee1bc18d7bb5688a0e
SpanishSemiconductor <montaz47@outlook.com>
2025-03-14 19:06:45 +0100

    poprawka lasowania

--- robot.cpp
+++ robot.cpp
@@ -9,72 +9,75 @@ const unsigned int SEGMENT_SIZE = 5;
 using namespace std;
 
 void wpiszDoMapy(int n, int m, int (*tablicaSegmentow[6])[SEGMENT_SIZE], int mapa[MAPA_SZER][MAPA_WYS]) {
-    srand(time(0));
-    int losowa = rand() % 6;
-    for (int i = 0; i < SEGMENT_SIZE; i++) {
-        for (int j = 0; j < SEGMENT_SIZE; j++) {
-            mapa[i + n][j + m] = (tablicaSegmentow[losowa])[i][j];
-        }
-    }
-    
+	 int losowa = rand() % 6;
+	for (int i = 0; i < SEGMENT_SIZE; i++) {
+		for (int j = 0; j < SEGMENT_SIZE; j++) {
+			mapa[i + n][j + m] = (tablicaSegmentow[losowa])[i][j];
+		}
+	}
+
 }
 void printMapa(int mapa[MAPA_SZER][MAPA_WYS]) {
-    for (int i = 0; i < MAPA_WYS; i++) {
-        for (int j = 0; j < MAPA_SZER; j++) {
-            cout <<setw(3)<< mapa[i][j];
-        }
-        cout << endl;
-    }
+	for (int i = 0; i < MAPA_WYS; i++) {
+		for (int j = 0; j < MAPA_SZER; j++) {
+			cout << setw(3) << mapa[i][j];
+		}
+		cout << endl;
+	}
 }
 int main()
 {
-    int mapa[MAPA_SZER][MAPA_WYS] = {};
-    int segmentA[SEGMENT_SIZE][SEGMENT_SIZE] = {
-        {0,0,1,0,0},
-        {0,1,1,0,0},
-        {1,1,0,1,1},
-        {0,1,1,1,0},
-        {0,0,1,0,0}
-    };
-    int segmentB[SEGMENT_SIZE][SEGMENT_SIZE] = {
-        {0,0,1,1,0},
-        {1,0,0,1,0},
-        {1,1,1,1,1},
-        {0,1,1,1,0},
-        {0,0,1,1,0}
-    };
-    int segmentC[SEGMENT_SIZE][SEGMENT_SIZE] = {
-        {1,1,1,1,1},
-        {1,1,0,1,1},
-        {1,1,0,0,1},
-        {0,1,0,0,0},
-        {0,1,1,1,1}
-    };
-    int segmentD[SEGMENT_SIZE][SEGMENT_SIZE] = {
-        {0,0,1,1,1},
-        {0,1,1,0,1},
-        {1,1,0,1,1},
-        {1,0,1,1,0},
-        {0,1,1,0,0}
-    };
-    int segmentE[SEGMENT_SIZE][SEGMENT_SIZE] = {
-        {1,1,1,0,0},
-        {0,1,1,1,0},
-        {1,1,0,1,1},
-        {1,1,0,1,1},
-        {1,1,1,1,0}
-    };
-    int segmentF[SEGMENT_SIZE][SEGMENT_SIZE] = {
-        {0,0,1,0,0},
-        {0,0,1,0,0},
-        {1,1,1,1,1},
-        {0,0,1,0,0},
-        {0,0,1,0,0}
-    };
-    int (*tabSegmentow[6])[SEGMENT_SIZE] = {segmentA, segmentB,segmentC, segmentD,segmentE,segmentF};
-    wpiszDoMapy(0, 0, tabSegmentow, mapa);
-    printMapa(mapa);
-
+	srand(time(0));
+	int mapa[MAPA_SZER][MAPA_WYS] = {};
+	int segmentA[SEGMENT_SIZE][SEGMENT_SIZE] = {
+		{0,0,1,0,0},
+		{0,1,1,0,0},
+		{1,1,0,1,1},
+		{0,1,1,1,0},
+		{0,0,1,0,0}
+	};
+	int segmentB[SEGMENT_SIZE][SEGMENT_SIZE] = {
+		{0,0,1,1,0},
+		{1,0,0,1,0},
+		{1,1,1,1,1},
+		{0,1,1,1,0},
+		{0,0,1,1,0}
+	};
+	int segmentC[SEGMENT_SIZE][SEGMENT_SIZE] = {
+		{1,1,1,1,1},
+		{1,1,0,1,1},
+		{1,1,0,0,1},
+		{0,1,0,0,0},
+		{0,1,1,1,1}
+	};
+	int segmentD[SEGMENT_SIZE][SEGMENT_SIZE] = {
+		{0,0,1,1,1},
+		{0,1,1,0,1},
+		{1,1,0,1,1},
+		{1,0,1,1,0},
+		{0,1,1,0,0}
+	};
+	int segmentE[SEGMENT_SIZE][SEGMENT_SIZE] = {
+		{1,1,1,0,0},
+		{0,1,1,1,0},
+		{1,1,0,1,1},
+		{1,1,0,1,1},
+		{1,1,1,1,0}
+	};
+	int segmentF[SEGMENT_SIZE][SEGMENT_SIZE] = {
+		{0,0,1,0,0},
+		{0,0,1,0,0},
+		{1,1,1,1,1},
+		{0,0,1,0,0},
+		{0,0,1,0,0}
+	};
+	int (*tabSegmentow[6])[SEGMENT_SIZE] = { segmentA, segmentB,segmentC, segmentD,segmentE,segmentF };
+	for (int j = 0; j < 20; j += 5) {
+		for (int i = 0; i < 40; i += 5) {
+			wpiszDoMapy(j, i, tabSegmentow, mapa);
+		}
+	}
+	printMapa(mapa);
 
 }
 
